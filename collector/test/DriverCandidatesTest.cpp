5c2611e9c71b89b43d8a2db2dd6cfd7501784eb7
Kirk Crane <kcrane@redhat.com>
2023-07-25 17:17:52 -0400

    force Core eBPF on s390x RHEL 8.4 kernels

--- collector/test/DriverCandidatesTest.cpp
+++ collector/test/DriverCandidatesTest.cpp
@@ -51,9 +51,10 @@ TEST(getGardenLinuxCandidateTest, Garden576_1) {
   MockHostInfoLocal host;
   std::string release("5.10.0-9-cloud-amd64");
   std::string version("#1 SMP Debian 5.10.83-1gardenlinux1 (2021-12-03)");
+  std::string machine("x86_64");
   std::string expected_driver("collector-ebpf-5.10.0-9-cloud-amd64-gl-5.10.83-1gardenlinux1.o");
   std::string expected_path("/kernel-modules");
-  KernelVersion kv(release, version);
+  KernelVersion kv(release, version, machine);
 
   EXPECT_CALL(host, GetKernelVersion()).WillOnce(Return(kv));
 
@@ -70,9 +71,10 @@ TEST(getGardenLinuxCandidateTest, Garden318) {
   MockHostInfoLocal host;
   std::string release("5.4.0-6-cloud-amd64");
   std::string version("#1 SMP Debian 5.4.93-1 (2021-02-09)");
+  std::string machine("x86_64");
   std::string expected_driver("collector-ebpf-5.4.0-6-cloud-amd64-gl-5.4.93-1.o");
   std::string expected_path("/kernel-modules");
-  KernelVersion kv(release, version);
+  KernelVersion kv(release, version, machine);
 
   EXPECT_CALL(host, GetKernelVersion()).WillOnce(Return(kv));
 
@@ -89,10 +91,11 @@ TEST(getMinikubeCandidateTest, v1_27_1) {
   MockHostInfoLocal host;
   std::string release("5.10.57");
   std::string version("#1 SMP Wed Oct 27 22:52:27 UTC 2021 x86_64 GNU/Linux");
+  std::string machine("x86_64");
   std::string minikube_version("v1.27.1");
   std::string expected_driver("collector-ebpf-5.10.57-minikube-v1.27.1.o");
   std::string expected_path("/kernel-modules");
-  KernelVersion kv(release, version);
+  KernelVersion kv(release, version, machine);
 
   EXPECT_CALL(host, GetMinikubeVersion()).WillOnce(Return(minikube_version));
   EXPECT_CALL(host, GetKernelVersion()).WillOnce(Return(kv));
@@ -110,10 +113,11 @@ TEST(getMinikubeCandidateTest, v1_24_0) {
   MockHostInfoLocal host;
   std::string release("4.19.202");
   std::string version("#1 SMP Wed Oct 27 22:52:27 UTC 2021 x86_64 GNU/Linux");
+  std::string machine("x86_64");
   std::string minikube_version("v1.24.0");
   std::string expected_driver("collector-ebpf-4.19.202-minikube-v1.24.0.o");
   std::string expected_path("/kernel-modules");
-  KernelVersion kv(release, version);
+  KernelVersion kv(release, version, machine);
 
   EXPECT_CALL(host, GetMinikubeVersion()).WillOnce(Return(minikube_version));
   EXPECT_CALL(host, GetKernelVersion()).WillOnce(Return(kv));
@@ -131,8 +135,9 @@ TEST(getMinikubeCandidateTest, NoVersion) {
   MockHostInfoLocal host;
   std::string release("4.19.202");
   std::string version("#1 SMP Wed Oct 27 22:52:27 UTC 2021 x86_64 GNU/Linux");
+  std::string machine("x86_64");
   std::string minikube_version("");
-  KernelVersion kv(release, version);
+  KernelVersion kv(release, version, machine);
 
   EXPECT_CALL(host, GetMinikubeVersion()).WillOnce(Return(minikube_version));
 
@@ -171,8 +176,9 @@ TEST(normalizeReleaseStringTest, FedoraKernel) {
   MockHostInfoLocal host;
   std::string release("5.14.18-300.fc35.x86_64");
   std::string version("#1 SMP Fri Nov 12 16:43:17 UTC 2021");
+  std::string machine("x86_64");
   std::string expected_kernel(release);
-  KernelVersion kv(release, version);
+  KernelVersion kv(release, version, machine);
 
   EXPECT_CALL(host, GetKernelVersion()).WillOnce(Return(kv));
   EXPECT_CALL(host, IsCOS()).WillOnce(Return(false));
@@ -187,10 +193,11 @@ TEST(normalizeReleaseStringTest, COSKernel) {
   MockHostInfoLocal host;
   std::string release("5.10.68+");
   std::string version("#1 SMP Fri Dec 3 10:04:10 UTC 2021");
+  std::string machine("x86_64");
   std::string build("build");
   std::string os_id("os");
   std::string expected_kernel("5.10.68-build-os");
-  KernelVersion kv(release, version);
+  KernelVersion kv(release, version, machine);
 
   EXPECT_CALL(host, GetKernelVersion()).WillOnce(Return(kv));
   EXPECT_CALL(host, IsCOS()).WillOnce(Return(true));
@@ -206,8 +213,9 @@ TEST(normalizeReleaseStringTest, DockerDesktopKernel) {
   MockHostInfoLocal host;
   std::string release("5.10.47-linuxkit");
   std::string version("#1 SMP Sat Jul 3 21:51:47 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux");
+  std::string machine("x86_64");
   std::string expected_kernel("5.10.47-dockerdesktop-2021-07-03-21-51-47");
-  KernelVersion kv(release, version);
+  KernelVersion kv(release, version, machine);
 
   EXPECT_CALL(host, GetKernelVersion()).WillOnce(Return(kv));
   EXPECT_CALL(host, IsCOS()).WillOnce(Return(false));
@@ -222,8 +230,9 @@ TEST(normalizeReleaseStringTest, GardenKernel) {
   MockHostInfoLocal host;
   std::string release("5.10.0-9-cloud-amd64");
   std::string version("#1 SMP Debian 5.10.83-1gardenlinux1 (2021-12-03)");
+  std::string machine("x86_64");
   std::string expected_kernel("5.10.0-9-cloud-amd64");
-  KernelVersion kv(release, version);
+  KernelVersion kv(release, version, machine);
 
   EXPECT_CALL(host, GetKernelVersion()).WillOnce(Return(kv));
   EXPECT_CALL(host, IsCOS()).WillOnce(Return(false));
@@ -238,8 +247,9 @@ TEST(normalizeReleaseStringTest, Garden318Kernel) {
   MockHostInfoLocal host;
   std::string release("5.4.0-6-cloud-amd64");
   std::string version("#1 SMP Debian 5.4.93-1 (2021-02-09)");
+  std::string machine("x86_64");
   std::string expected_kernel("5.4.0-6-cloud-amd64");
-  KernelVersion kv(release, version);
+  KernelVersion kv(release, version, machine);
 
   EXPECT_CALL(host, GetKernelVersion()).WillOnce(Return(kv));
   EXPECT_CALL(host, IsCOS()).WillOnce(Return(false));
